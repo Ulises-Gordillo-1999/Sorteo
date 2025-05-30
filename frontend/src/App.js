a9e232f0c2a6071977775366ba2dea15081c5a05
ulises <uligordillo@gmail.com>
2025-05-29 21:47:43 -0300

    se elimino el navbar del proyecto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Aspirantes from "./pages/Aspirantes";
 import Sorteo from "./pages/Sorteo";
 import FormularioAspirante from "./components/FormularioAspirante";
